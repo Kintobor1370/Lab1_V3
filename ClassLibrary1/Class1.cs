594754844f3008a3fe597a655b9624385fdb97c8
Leonid Inozemtsev <92368620+Kintobor1370@users.noreply.github.com>
2022-04-25 09:38:32 +0300

    Add files via upload

---
+++ ClassLibrary1/Class1.cs
@@ -0,0 +1,183 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.Collections.Specialized;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+
+namespace ClassLibrary1
+{
+    //.........................МАТЕМАТИЧЕСКИЕ ФУНКЦИИ ИЗ ПАКЕТА ВЕКТОРОНОЙ МАТЕМАТИКИ INTEL MKL
+    public enum VMf
+    {
+        vmsLn,
+        vmdLn,
+        vmsLGamma,
+        vmdLGamma
+    }
+
+    //.........................СЕТКА, УЗЛЫ КТОРОЙ ЯВЛЯЮТСЯ АРГУМЕНТАМИ ФУНКЦИИ
+    [Serializable]
+    public class VMGrid
+    {
+        public int Num { get; set; }
+        public float[] Scope { get; set; }
+        public VMf Func { get; set; }
+        public float Step { get { return (Scope[1] - Scope[0]) / (Num - 1); } }
+
+        public VMGrid(int n = 2, float min = 0, float max = 1, VMf f = VMf.vmsLn)
+        {
+            this.Num = n;
+            this.Scope = new float[2];
+            this.Scope[0] = min;
+            this.Scope[1] = max;
+            Func = f;
+        }
+    }
+
+    //.........................РЕЗУЛЬТАТЫ СРАВНЕНИЯ ВРЕМЕНИ ВЫЧИСЛЕНИЙ В РЕЖИМАХ VML_HA И VML_EP
+    [Serializable]
+    public struct VMTime
+    {
+        public VMGrid VMG_Data { get; set; }
+        public int HA_Time { get; set; }                                                                  // время вычислений с точностью VML_HA
+        public int EP_Time { get; set; }                                                                  // время вычислений с точностью VML_EP
+
+        public VMTime(VMGrid g, int HA, int EP)
+        {
+            this.VMG_Data = g;
+            this.HA_Time = HA;
+            this.EP_Time = EP;
+        }
+
+        public float EP_by_HA
+        {
+            get
+            {
+                float res = 0;
+                if (HA_Time != 0) res = EP_Time / HA_Time;
+                return res;
+            }
+        }
+
+        public override string ToString()
+        {
+            return $"Argument Vector Length: {VMG_Data.Num};" +
+                   $"Min: {VMG_Data.Scope[0]}; Max: {VMG_Data.Scope[1]};" +
+                   $"Step: {VMG_Data.Step};" +
+                   $"VM Fuction: {VMG_Data.Func};" +
+                   $"HA calculation time: {HA_Time}; HA calculation time: {EP_Time};" +
+                   $"EP time by HA time: {EP_by_HA}";
+        }
+    }
+
+    //.........................РЕЗУЛЬТАТЫ СРАВНЕНИЯ ТОЧНОСТИ ВЫЧИСЛЕНИЙ В РЕЖИМАХ VML_HA И VML_EP
+    [Serializable]
+    public struct VMAccuracy
+    {
+        public VMGrid VMG_Data { get; set; }
+        public float Max_arg { get; set; }                                                                // значения аргумента, при котором максимально отличаются значения функии в режимах VML_HA и VML_EP
+        public float HA_Val { get; set; }                                                                 // значение функции с аргументом Max_arg в режиме VML_HA
+        public float EP_Val { get; set; }                                                                 // значение функции с аргументом Max_arg в режиме VML_EP
+
+        public VMAccuracy(VMGrid g, float arg, float HA, float EP)
+        {
+            this.VMG_Data = g;
+            this.Max_arg = arg;
+            this.HA_Val = HA;
+            this.EP_Val = EP;
+        }
+
+        public float Max_Sub_Val { get { return Math.Abs(EP_Val - HA_Val); } }
+
+        public override string ToString()
+        {
+            return $"Argument Vector Length: {VMG_Data.Num};" +
+                   $"Min: {VMG_Data.Scope[0]}; Max: {VMG_Data.Scope[1]};" +
+                   $"Step: {VMG_Data.Step};" +
+                   $"VM Fuction: {VMG_Data.Func};" +
+                   $"Max subtraction argument: {Max_arg}; HA value: {HA_Val}; EP value: {EP_Val};" +
+                   $"Max subtraction value: {Max_Sub_Val}";
+        }
+    }
+
+    //.........................СОЗДАНИЕ КОЛЛЕКЦИЙ VMTime И VMAccuracy
+    [Serializable]
+    public class VMBenchmark
+    {
+        [DllImport("C:\\Users\\User\\Desktop\\prog\\C#\\Sem6\\Lab1_V3\\x64\\Debug\\Dll1.dll")]
+        static extern void VM(int n, float Min_Arg, float Max_Arg, int VMF_num, int[] Time_Data, float[] Accuracy_Data);
+        public ObservableCollection<VMTime> TimeCollection { get; set; }
+        public ObservableCollection<VMAccuracy> AccuracyCollection { get; set; }
+
+        public VMBenchmark()
+        {
+            TimeCollection = new ObservableCollection<VMTime>();
+            AccuracyCollection = new ObservableCollection<VMAccuracy>();
+        }
+
+        public void AddVMTime(VMGrid g)
+        {
+            VMGrid Copy = new VMGrid(g.Num, g.Scope[0], g.Scope[1], g.Func);
+            int[] Time_Data = new int[2];
+            float[] Accuracy_Data = new float[3];
+
+            VM(Copy.Num, Copy.Scope[0], Copy.Scope[1], (int)Copy.Func, Time_Data, Accuracy_Data);
+            var New_VMTime = new VMTime(Copy, Time_Data[0], Time_Data[1]);
+            Console.WriteLine(New_VMTime.ToString());
+            TimeCollection.Add(New_VMTime);
+        }
+
+        public void AddVMAccuracy(VMGrid g)
+        {
+            VMGrid Copy = new VMGrid(g.Num, g.Scope[0], g.Scope[1], g.Func);
+            int[] Time_Data = new int[2];
+            float[] Accuracy_Data = new float[3];
+
+            VM(Copy.Num, Copy.Scope[0], Copy.Scope[1], (int)Copy.Func, Time_Data, Accuracy_Data);
+            var New_VMAccuracy = new VMAccuracy(Copy, Accuracy_Data[0], Accuracy_Data[1], Accuracy_Data[2]);
+            AccuracyCollection.Add(New_VMAccuracy);
+        }
+
+        public float EP_by_HA_Max
+        {
+            get
+            {
+                float max = 0;
+                for (int i = 0; i < TimeCollection.Count; i++)
+                { if (max < TimeCollection[i].EP_by_HA) max = TimeCollection[i].EP_by_HA; }
+                return max;
+            }
+        }
+
+        public float EP_by_HA_Min
+        {
+            get
+            {
+                try
+                {
+                    float min = TimeCollection[0].EP_by_HA;
+                    for (int i = 1; i < TimeCollection.Count; i++)
+                    { if (min > TimeCollection[i].EP_by_HA) min = TimeCollection[i].EP_by_HA; }
+                    return min;
+                }
+                catch
+                { return 0; }
+            }
+        }
+
+        public void Display()
+        {
+            Console.WriteLine("_________________VMTime Collection:_______________");
+            for (int i = 0; i < TimeCollection.Count; i++)
+                Console.WriteLine($"Element {i + 1}:\n" + TimeCollection[i].ToString() + "\n");
+
+            Console.WriteLine("_______________VMAccuracy Collection:_____________");
+            for (int i = 0; i < AccuracyCollection.Count; i++)
+                Console.WriteLine($"Element {i + 1}:\n" + TimeCollection[i].ToString() + "\n");
+
+            Console.WriteLine("__________________________________________________");
+        }
+    }
+}
---
+++ WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,348 @@
+﻿using System;
+using System.IO;
+using System.ComponentModel;
+using System.Collections.Specialized;
+using System.Collections.ObjectModel;
+using System.Windows;
+using System.Windows.Data;
+using System.Runtime.CompilerServices;
+using System.Runtime.Serialization.Formatters.Binary;
+using ClassLibrary1;
+
+namespace WpfApp1
+{
+    //.........................ПРИВЯЗКА ДАННЫХ ОБЪЕКТА VMBenchmark К ЭЛЕМЕНТАМ УПРАВЛЕНИЯ ПРИЛОЖЕНИЯ
+    public class ViewData : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        public VMGrid VMGrid { get; set; }
+        public VMBenchmark VMBenchmark { get; set; }
+        private bool data_changed = false;
+
+        public ViewData()
+        {
+            this.VMBenchmark = new VMBenchmark();
+            this.VMGrid = new VMGrid();
+        }
+
+        public ViewData(VMBenchmark b)
+        {
+            this.VMBenchmark = b;
+            this.VMGrid = new VMGrid();
+
+            VMBenchmark.TimeCollection.CollectionChanged += TimeCollectionChanged;
+            VMBenchmark.AccuracyCollection.CollectionChanged += AccuracyCollectionChanged;
+        }
+
+        public void AddVMTime(VMGrid NewGrid)
+        {
+            try
+            {
+                VMBenchmark.AddVMTime(NewGrid);
+                OnPropertyChanged("EP_by_HA_Max");
+                OnPropertyChanged("EP_by_HA_Min");
+            }
+            catch (Exception ex)
+            { MessageBox.Show("AN EXCEPTION HAS OCCURRED:\n" + ex.ToString(), "Error", MessageBoxButton.OK, MessageBoxImage.Error); }
+        }
+
+        public void AddVMAccuracy(VMGrid NewGrid)
+        {
+            try
+            { VMBenchmark.AddVMAccuracy(NewGrid); }
+            catch (Exception ex)
+            { MessageBox.Show("AN EXCEPTION HAS OCCURRED:\n" + ex.ToString(), "Error", MessageBoxButton.OK, MessageBoxImage.Error); }
+        }
+
+        public float EP_by_HA_Max
+        { get { return VMBenchmark.EP_by_HA_Max; } }
+
+        public float EP_by_HA_Min
+        { get { return VMBenchmark.EP_by_HA_Min; } }
+
+        public void Save(string filename)
+        {
+            BinaryFormatter Formatter = new BinaryFormatter();
+            FileStream fs = null;
+            try
+            {
+                using (fs = new FileStream(filename, FileMode.OpenOrCreate))
+                {
+                    Formatter.Serialize(fs, VMBenchmark);
+                    Console.WriteLine("\nSerialization Complete");
+                }
+            }
+            catch (Exception ex)
+            { Console.WriteLine("AN EXCEPTION HAS OCCURRED:\n" + ex.Message); }
+            finally
+            { if (fs != null) fs.Close(); }
+        }
+
+        public void Load(string filename)
+        {
+            BinaryFormatter Formatter = new BinaryFormatter();
+            FileStream fs = null;
+            try
+            {
+                using (fs = new FileStream(filename, FileMode.Open))
+                {
+                    VMBenchmark LoadedBenchmark = (VMBenchmark)Formatter.Deserialize(fs);
+                    for (int i = 0; i < VMBenchmark.TimeCollection.Count; i++)
+                        this.VMBenchmark.TimeCollection[i] = LoadedBenchmark.TimeCollection[i];
+                    for(int i = 0; i < VMBenchmark.AccuracyCollection.Count; i++)
+                        this.VMBenchmark.AccuracyCollection[i] = LoadedBenchmark.AccuracyCollection[i];
+
+                    Console.WriteLine("\nDeserialization Complete");
+                }
+            }
+            catch (Exception ex)
+            { Console.WriteLine("AN EXCEPTION HAS OCCURRED:\n" + ex.Message); }
+            finally
+            { if (fs != null) fs.Close(); }
+        }
+
+        public bool IsChanged
+        {
+            get
+            { return data_changed; }
+            set
+            { data_changed = value; OnPropertyChanged("IsChanged"); }
+        }
+
+        public void OnPropertyChanged([CallerMemberName] string prop = "")
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(prop));
+        }
+
+        void TimeCollectionChanged(object? sender, NotifyCollectionChangedEventArgs e)
+        { OnPropertyChanged("VMBenchmark.TimeCollection"); }
+
+        void AccuracyCollectionChanged(object? sender, NotifyCollectionChangedEventArgs e)
+        { OnPropertyChanged("VMBenchmark.AccuracyCollection"); }
+    }
+
+
+    //.........................ГЛАВНОЕ ОКНО ПРИЛОЖЕНИЯ WPF
+    public partial class MainWindow : Window
+    {
+        public ViewData? ViewData;
+        Microsoft.Win32.SaveFileDialog save;
+        string name = "Your_file_name_please";
+        string ext = ".bin";
+        string save_message = "Все несохраненные данные будут потеряны.\nЖелаеете сохранить их?";
+
+        public MainWindow()
+        {
+            save = new Microsoft.Win32.SaveFileDialog();
+            save.FileName = name;
+            save.DefaultExt = ext;
+
+            InitializeComponent();
+            
+            VMBenchmark VMBenchmark = new VMBenchmark();
+            ViewData = new ViewData(VMBenchmark);
+            
+            this.DataContext = ViewData;
+            WMfBox.ItemsSource = Enum.GetValues(typeof(VMf));
+            TimeCollectionList.ItemsSource = VMBenchmark.TimeCollection;
+            AccuracyCollectionList.ItemsSource = VMBenchmark.AccuracyCollection;
+        }
+        
+        private void New_Click(object sender, RoutedEventArgs e)
+        {
+            if (ViewData.IsChanged)
+            {
+                MessageBoxResult result = MessageBox.Show(save_message, "Save?", MessageBoxButton.YesNo, MessageBoxImage.Warning, MessageBoxResult.Yes);
+                if (result == MessageBoxResult.Yes)
+                {
+                    if (save.ShowDialog() == true)
+                    {
+                        if (ViewData != null)
+                            ViewData.Save(save.FileName);
+                    }
+                }
+                ViewData.IsChanged = false;
+            }
+            ViewData.VMBenchmark.TimeCollection.Clear();
+            ViewData.VMBenchmark.AccuracyCollection.Clear();
+        }
+        
+        private void Save_Click(object sender, RoutedEventArgs e)
+        {
+            if (save.ShowDialog() == true)
+            {
+                if (ViewData != null)
+                    ViewData.Save(save.FileName);
+            }
+            ViewData.IsChanged = false;
+        }
+        
+        private void Open_Click(object sender, RoutedEventArgs e)
+        {
+            if (ViewData.IsChanged)
+            {
+                MessageBoxResult result = MessageBox.Show(save_message, "Save?", MessageBoxButton.YesNo, MessageBoxImage.Warning, MessageBoxResult.Yes);
+                if (result == MessageBoxResult.Yes)
+                {
+                    Microsoft.Win32.SaveFileDialog new_save = new Microsoft.Win32.SaveFileDialog();
+                    new_save.FileName = name;
+                    new_save.DefaultExt = ".txt";
+
+                    if (new_save.ShowDialog() == true)
+                    {
+                        if (ViewData != null)
+                            ViewData.Save(new_save.FileName);
+                    }
+                }
+                ViewData.IsChanged = false;
+            }
+            ViewData.VMBenchmark.TimeCollection.Clear();
+            ViewData.VMBenchmark.AccuracyCollection.Clear();
+
+            Microsoft.Win32.OpenFileDialog open = new Microsoft.Win32.OpenFileDialog();
+            open.FileName = name;
+            open.DefaultExt = ".txt";
+
+            if (open.ShowDialog() == true)
+                ViewData.Load(open.FileName);
+        }
+        
+        private void AddVMTime_Click(object sender, RoutedEventArgs e)
+        {
+            ViewData.AddVMTime(ViewData.VMGrid);
+            ViewData.OnPropertyChanged("VMBenchmark.TimeCollection");
+            ViewData.IsChanged = true;
+        }
+        
+        private void AddVMAccuracy_Click(object sender, RoutedEventArgs e)
+        {
+            ViewData.AddVMAccuracy(ViewData.VMGrid);
+            ViewData.OnPropertyChanged("VMBenchmark.AccuracyCollection");
+            ViewData.IsChanged = true;
+        }
+        
+        void WpfApp1_Closing(object sender, CancelEventArgs e)
+        {
+            if (ViewData.IsChanged)
+            {
+                MessageBoxResult result = MessageBox.Show(save_message, "Save?", MessageBoxButton.YesNo, MessageBoxImage.Warning, MessageBoxResult.Yes);
+                if (result == MessageBoxResult.Yes)
+                {
+                    if (save.ShowDialog() == true)
+                    {
+                        if (ViewData != null)
+                            ViewData.Save(save.FileName);
+                    }
+                }
+                ViewData.IsChanged = false;
+            }
+        }
+    }
+
+
+    //.........................КОНВЕРТЕРЫ
+    public class NumConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            try
+            {
+                string Num = value.ToString();
+                return Num;
+            }
+            catch
+            { return "EX"; }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            object obj = null;
+            try
+            {
+                string str = value as string;
+                int Num;
+                Int32.TryParse(str, out Num);
+
+                if (Num == 0)
+                { Exception EX = new Exception(); throw (EX); }
+
+                obj = Num;
+                return obj;
+            }
+            catch
+            { return obj; }
+        }
+    }
+
+    public class ScopeConverter : IMultiValueConverter
+    {
+        public object Convert(object[] values, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            try
+            {
+                string Min = values[0].ToString();
+                string Max = values[1].ToString();
+                return Min + ";" + Max;
+            }
+            catch
+            { return "EX"; }
+        }
+        public object[] ConvertBack(object values, Type[] targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            object[] obj = new object[2];
+            try
+            {
+                string str_vals = values as string;
+                string[] vals = str_vals.Split(';', StringSplitOptions.RemoveEmptyEntries);
+                float Min = float.Parse(vals[0]);
+                float Max = float.Parse(vals[1]);
+
+                if (Min > Max)
+                { Exception EX = new Exception(); throw (EX); }
+
+                obj[0] = Min;
+                obj[1] = Max;
+                return obj;
+            }
+            catch
+            { return obj; }
+        }
+    }
+
+    public class FloatConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            try
+            {
+                float val = (float)value;
+                return val.ToString();
+            }
+            catch
+            { return "EX"; }
+        }
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        { throw new NotImplementedException(); }
+    }
+
+    public class BoolConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            try
+            {
+                if ((bool)value)
+                    return "Данные были изменены";
+                else
+                    return "";
+            }
+            catch
+            { return "EX"; }
+        }
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
